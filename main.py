5d996adf45d1dfeacc754c6b6200b6fbaeae2121
Juuso Reunamo <juuso.reunamo@gmail.com>
2026-09-11 19:37:12 +0300

    Updated code in main.py so the test_main.py runs

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 print ("Calculate the area of a wall.")
-Feed = (input("Enter the width in meters: "))
-Width = float(Feed)
-Feed = input("Enter the height in meters:")
-Height = float(Feed)
+while True:     # Check for valid integer input for the width
+    try:
+        Feed = input("Enter the width in meters:")
+        Width = int(Feed)
+        break
+    except ValueError:
+        print("Please enter a valid integer for width.")
+while True:     # Check for valid integer input for the height
+    try:
+        Feed = input("Enter the height in meters:")
+        Height = int(Feed)
+        break
+    except ValueError:
+        print("Please enter a valid integer for height.")
 Area = Width * Height
 print (f"Width is {Width}m and height is {Height}m.")
-print (f"The wall will be {round(Area, 2)} square meters.")
+print (f"The wall will be {Area} square meters.")
